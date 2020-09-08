851f91168020a4628a4f7b550dbdd620ce638783
Edward Birdsall <edward.birdsall@postgrad.manchester.ac.uk>
2020-09-08 01:12:36 +0100

    2020-09-08 -> proton (ramdisk version)

--- src/filestripper.C
+++ src/filestripper.C
@@ -739,7 +739,7 @@ void filestripper()
 
     // TODO: change output file name
 
-    std::cout << "last chance to check important parameters" << std::endl
+    std::cout << "last chance to check important parameters" << std::endl;
     std::cout << "Run_min=" << Run_min << " Run_max=" << Run_max << std::endl;
     std::cout << "Output file directory: " << foutput_dir << std::endl;
     std::cout << "Output file name: " << foutput_fname << std::endl;
